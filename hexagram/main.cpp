76b35d46a5a7f95704881849cdd14924d0edf790
Yuzo <hyzgog@gmail.com>
2015-06-22 12:05:23 +0800

    basic hexagram

---
+++ hexagram/main.cpp
@@ -0,0 +1,125 @@
+#include <cmath>
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <boost/format.hpp>
+#include "../common/util.hpp"
+
+using namespace std;
+using namespace boost;
+
+int main(int argc, char* argv[]) {
+	if (!glfwInit()) {
+		throw runtime_error("Failed to initialize GLFW");
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Hexagram", NULL, NULL);
+	if (!window) {
+		throw runtime_error("Failed to open GLFW window");
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		throw runtime_error("Failed to initialize GLEW");
+	}
+
+	GLuint programID = buildProgram("vertex.shader", "fragment.shader");
+	glUseProgram(programID);
+
+	GLuint vertexArrayID;
+	glGenVertexArrays(1, &vertexArrayID);
+	glBindVertexArray(vertexArrayID);
+
+	const int star = 6;
+	GLfloat vertexData[star * 12];
+
+	for (int i = 0; i < star; i++) {
+		vertexData[12 * i] = cos(2 * i * M_PI / star) / 2;
+		vertexData[12 * i + 1] = sin(2 * i * M_PI / star) / 2;
+
+		vertexData[12 * i + 2] = cos(2 * (i + 1) * M_PI / star) / 2;
+		vertexData[12 * i + 3] = sin(2 * (i + 1) * M_PI / star) / 2;
+
+		vertexData[12 * i + 4] = 0;
+		vertexData[12 * i + 5] = 0;
+
+
+		vertexData[12 * i + 6] = cos(2 * i * M_PI / star) / 2;
+		vertexData[12 * i + 7] = sin(2 * i * M_PI / star) / 2;
+
+		vertexData[12 * i + 8] = cos(2 * (i + 1) * M_PI / star) / 2;
+		vertexData[12 * i + 9] = sin(2 * (i + 1) * M_PI / star) / 2;
+
+		vertexData[12 * i + 10] = cos((2 * i + 1) * M_PI / star);
+		vertexData[12 * i + 11] = sin((2 * i + 1) * M_PI / star);
+	}
+
+	static const GLfloat colorData[] = { 
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	GLuint colorBuffer;
+	glGenBuffers(1, &colorBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colorData), colorData, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(1);
+
+	do {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, star * 2 * 3 * 2);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+	return EXIT_SUCCESS;
+}
---
+++ hexagram/vertex.shader
@@ -0,0 +1,11 @@
+#version 330 core
+
+layout(location = 0) in vec4 vPosition;
+layout(location = 1) in vec3 vColor;
+
+out vec3 fColor;
+
+void main() {
+	gl_Position = vPosition;
+	fColor = vColor;
+}
